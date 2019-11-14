fc21a23c2503a131a69858f5411f1d4dfb1fa21a
sfwsaladbar <55550980+sfwsaladbar@users.noreply.github.com>
2019-11-14 17:39:22 -0500

    Update TempHum_Test_Code.ino

--- TempHum_Test_Code/TempHum_Test_Code.ino
+++ TempHum_Test_Code/TempHum_Test_Code.ino
@@ -7,18 +7,21 @@
  * https://playground.arduino.cc/Main/DHTLib
  */
  
-#include <dht.h>
+#include <DHT.h>
+#define DHTTYPE DHT22
 
-#define dataPin 8 // Defines pin number to which the sensor is connected
-dht DHT; // Creats a DHT object
+#define dataPin 2 // Defines pin number to which the sensor is connected
+DHT dht(dataPin, DHTTYPE); // Creats a DHT object
+
 
 void setup() {
   Serial.begin(9600);
+  dht.begin();
 }
 void loop() {
-  int readData = DHT.read22(dataPin); // Reads the data from the sensor
-  float t = DHT.temperature; // Gets the values of the temperature
-  float h = DHT.humidity; // Gets the values of the humidity
+  //int readData = dht.(dataPin); // Reads the data from the sensor
+  float t = dht.readTemperature(); // Gets the values of the temperature
+  float h = dht.readHumidity(); // Gets the values of the humidity
   
   // Printing the results on the serial monitor
   Serial.print("Temperature = ");

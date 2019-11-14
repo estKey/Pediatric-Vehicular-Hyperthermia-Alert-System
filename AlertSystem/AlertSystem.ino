dbf8f00def8db7854d5f06020a339915068adcf1
estKey <estkey@outlook.com>
2019-11-14 12:27:16 -0500

    add initial arduino template and circuit schematics

---
+++ AlertSystem/AlertSystem.ino
@@ -0,0 +1,11 @@
+/**
+ * Section 206 Group 02
+ * Department of Electrical & Computer Engineering 
+ * APSC 200/293 P2 Project: Preventing PediatricVehicular Hyperthermia
+ */
+
+void setup() {
+}
+
+void loop() {
+}
